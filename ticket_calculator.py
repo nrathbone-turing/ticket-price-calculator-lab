a868e7ae705c23de812fcc959772257dcdcd2f12
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-25 16:35:53 -0600

    Upload pseudocode for project plan

---
+++ ticket_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# psuedocode
+'''
+This script takes in two user inputs
+- movie type : (regular / premium) string
+- age : integer
+
+Then calculate...
+- the base cost of the movie using conditional statements
+- the base cost of the age using conditional statements
+
+Sum up the total cost
+Display the result using an f-string
+'''
+
