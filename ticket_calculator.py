deca8af7ffddc22fbdfa581ff912ded1199cef44
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-25 16:43:44 -0600

    Create function to determine which age group user is in

--- ticket_calculator.py
+++ ticket_calculator.py
@@ -3,14 +3,25 @@
 # psuedocode
 '''
 This script takes in two user inputs
+- age group: integer
 - movie type : (regular / premium) string
-- age : integer
 
 Then calculate...
-- the base cost of the movie using conditional statements
 - the base cost of the age using conditional statements
+- the base cost of the movie type using conditional statements
 
 Sum up the total cost
 Display the result using an f-string
 '''
+
+# makes more sense to use functions for this one to make testing easier
+
+# first function to determine which age group the user falls in
+def age_group(age: int) -> str:
+    if age <= 12:
+        return "child"
+    elif age >= 60:
+        return "senior"
+    else:
+        return "adult"
 
